d6fe1a757cae218ea7339a32508eeb45c7ef2114
chriswynn <chris@wynncodes.com>
2020-03-17 17:35:07 -0400

    feat: add app page

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { AppProps } from "next/app";
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
+
+export default App;
