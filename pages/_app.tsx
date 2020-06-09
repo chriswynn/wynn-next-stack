64bc7d2440151e985576043db8ba6e3150e4a88f
chriswynn <chris@wynncodes.com>
2020-06-08 20:58:40 -0400

    refactor: strip out typescript

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { AppProps } from "next/app";
-import "minireset.css";
-
-const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
-
-export default App;
--- pages/index.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-const IndexPage: React.FC = () => <div>Home Page</div>;
-
-export default IndexPage;
